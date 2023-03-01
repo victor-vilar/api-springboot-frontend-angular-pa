029237138af42fde48e643929d3182fb27f6590a
Victor Vilar <victor.svilar@gmail.com>
2023-03-01 14:10:56 -0300

    implementado o template e o componente de customer-supervisors .

--- src/app/components/crudbase/crud-components/customer/customer-supervisors/customer-supervisors.component.ts
+++ src/app/components/crudbase/crud-components/customer/customer-supervisors/customer-supervisors.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Customer } from 'src/app/model/Customer';
+import { AddressService } from 'src/app/services/address.service';
+import { CustomerService } from 'src/app/services/customer.service';
+import { SupervisorService } from 'src/app/services/supervisor.service';
 
 @Component({
   selector: 'app-customer-supervisors',
@@ -7,9 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerSupervisorsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    customerService:CustomerService,
+    supervisorService:SupervisorService,
+    private activeRoute:ActivatedRoute) {
+      this.supervisorService = supervisorService;
+      this.customerService = customerService;
+     }
+
+  selectedCustomer:Customer;
+  title='Fiscais'
+  pathPrefix='fiscais';
+  headerForTables:any;
+  pathToOperations = [{name:"+", path: this.pathPrefix + '/novo', title:"Novo " + this.pathPrefix}];
+  customerService:CustomerService;
+  supervisorService:SupervisorService;
 
   ngOnInit(): void {
+    this.headerForTables ={
+      id:'Id',
+      name:'Nome',
+      phoneNumber:'Telefone',
+      email:'Email',
+     };
+
+    this.activeRoute.paramMap.subscribe(param =>{
+      this.selectedCustomer = this.customerService.list.find(obj => obj.cpfCnpj === param.get('cpfCnpj'));
+    })
   }
 
 }
